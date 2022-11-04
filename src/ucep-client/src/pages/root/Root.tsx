934684ea1960b0f95d75a9ceb058380955794d68
Shashank-15 <Shashank.sohani@gigatorb.com>
2022-11-04 18:05:33 +0530

    FRONTEND: Added Header to Root page.

--- src/ucep-client/src/pages/root/Root.tsx
+++ src/ucep-client/src/pages/root/Root.tsx
@@ -3,10 +3,12 @@ import "./Root.css";
 import { Link } from "react-router-dom";
 
 import Button from "../../components/Button";
+import Header from "../../components/Header";
 
 const App = () => {
   return (
     <>
+      <Header />
       <h1 className="h-36 text-6xl text-center">UCEP</h1>
       <div className="flex flex-row justify-center">
         <Button>
