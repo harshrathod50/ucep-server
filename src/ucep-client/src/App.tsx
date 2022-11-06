b15a9c32b8ed9bc4eaacd339d58cc4d94c14fb3b
Harsh Rathod <hrathore50@ymail.com>
2022-11-06 08:52:12 +0530

    FRONTEND: Installed and integrated custom font families.

--- src/ucep-client/src/App.tsx
+++ src/ucep-client/src/App.tsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto-serif/400.css";
+import "@fontsource/roboto-mono/400.css";
 
 const App = () => {
   return <h1 className="text-3xl font-bold">Hello, World!</h1>;
